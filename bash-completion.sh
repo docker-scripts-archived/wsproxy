ea632258e2423e182fee386e1503865a97c1999e
Dashamir Hoxha <dashohoxha@gmail.com>
2017-07-31 09:26:42 +0200

    Fix bash-completion

--- bash-completion.sh
+++ bash-completion.sh
@@ -1,37 +1,23 @@
 # bash completions for the command `ds domains-add`
-_ds_domains-rm() {
-    local cur=${COMP_WORDS[COMP_CWORD]}   ## $1
-    local domains=$(cat containers.txt | cut -d: -f2)
-    COMPREPLY=( $(compgen -W "$domains" -- $cur) )
+_ds_domains-add() {
+    local containers=$(docker ps -a --format "{{.Names}}")
+    COMPREPLY=( $(compgen -W "$containers" -- $1) )
 }
 
-# bash completions for the command `ds domains-add`
-_ds_domains-add() {
-    COMPREPLY=()   # Array variable storing the possible completions.
-    local cur=${COMP_WORDS[COMP_CWORD]}     ## $1
-    local prev=${COMP_WORDS[COMP_CWORD-1]}  ## $2
-
-    if [[ $COMP_CWORD -eq 2 ]]; then
-        local containers=$(docker ps -a --format "{{.Names}}")
-        COMPREPLY=( $(compgen -W "$containers" -- $cur) )
-    fi
+# bash completions for the command `ds domains-rm`
+_ds_domains-rm() {
+    local domains=$(cat $(_ds_container_dir)/containers.txt | cut -d: -f2)
+    COMPREPLY=( $(compgen -W "$domains" -- $1) )
 }
 
 # bash completions for the command `ds get-ssl-cert`
 _ds_get-ssl-cert() {
-    COMPREPLY=()   # Array variable storing the possible completions.
-    local cur=${COMP_WORDS[COMP_CWORD]}     ## $1
-    local prev=${COMP_WORDS[COMP_CWORD-1]}  ## $2
-
-    if [[ $COMP_CWORD -gt 2 ]]; then
-        local domains=$(cat containers.txt | cut -d: -f2)
-        COMPREPLY=( $(compgen -W "$domains" -- $cur) )
-    fi
+    local domains=$(cat $(_ds_container_dir)/containers.txt | cut -d: -f2)
+    COMPREPLY=( $(compgen -W "$domains" -- $1) )
 }
 
 # bash completions for the command `ds sshtunnel-rm`
 _ds_sshtunnel-rm() {
-    local cur=${COMP_WORDS[COMP_CWORD]}   ## $1
     local domains=$(ls sshtunnel-keys/*.key | sed -e 's#sshtunnel-keys/##g' -e 's/.key//g')
-    COMPREPLY=( $(compgen -W "$domains" -- $cur) )
+    COMPREPLY=( $(compgen -W "$domains" -- $1) )
 }

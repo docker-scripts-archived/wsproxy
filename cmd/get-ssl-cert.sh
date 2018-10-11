0056f8e0de5e21f90ef449e2a845aa457a4548ac
dashohoxha <dashohoxha@gmail.com>
2018-10-11 12:18:13 +0200

    Fix get-ssl-cert.sh

--- cmd/get-ssl-cert.sh
+++ cmd/get-ssl-cert.sh
@@ -48,17 +48,17 @@ cmd_get-ssl-cert() {
         local certdir=/etc/letsencrypt/live/$domain
         # update apache2 config file in wsproxy
         sed -i sites-available/$domain.conf -r \
-            -e "s|#?SSLCertificateFile .*|SSLCertificateFile      $certdir/cert.pem|" \
-            -e "s|#?SSLCertificateKeyFile .*|SSLCertificateKeyFile   $certdir/privkey.pem|" \
-            -e "s|#?SSLCertificateChainFile .*|SSLCertificateChainFile $certdir/chain.pem|"
+            -e "s|#?SSLCertificateFile.*|SSLCertificateFile      $certdir/cert.pem|" \
+            -e "s|#?SSLCertificateKeyFile.*|SSLCertificateKeyFile   $certdir/privkey.pem|" \
+            -e "s|#?SSLCertificateChainFile.*|SSLCertificateChainFile $certdir/chain.pem|"
         # update apache2 config file in the container
         # it is done with the help of a tmp file in order to preserve the links
         local container=$(cat containers.txt | grep $domain | cut -d: -f1)
         docker exec $container sh -c "
             sed /etc/apache2/sites-available/$domain.conf -r \
-                -e 's|#?SSLCertificateFile .*|SSLCertificateFile      $certdir/cert.pem|' \
-                -e 's|#?SSLCertificateKeyFile .*|SSLCertificateKeyFile   $certdir/privkey.pem|' \
-                -e 's|#?SSLCertificateChainFile .*|SSLCertificateChainFile $certdir/chain.pem|' \
+                -e 's|#?SSLCertificateFile.*|SSLCertificateFile      $certdir/cert.pem|' \
+                -e 's|#?SSLCertificateKeyFile.*|SSLCertificateKeyFile   $certdir/privkey.pem|' \
+                -e 's|#?SSLCertificateChainFile.*|SSLCertificateChainFile $certdir/chain.pem|' \
                 > /etc/apache2/sites-available/$domain.conf.tmp ;
             cat /etc/apache2/sites-available/$domain.conf.tmp > /etc/apache2/sites-available/$domain.conf ;
             rm /etc/apache2/sites-available/$domain.conf.tmp

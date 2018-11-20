07f8644649b85aa7b7cd8c36cae10ca4f8488790
dashohoxha <dashohoxha@gmail.com>
2018-11-20 07:34:49 +0100

    Use your SMTP server for sending emails.

--- settings.sh
+++ settings.sh
@@ -6,6 +6,14 @@ CONTAINER=wsproxy
 PORT_SSH=2222    ## ssh port is needed for websites that are accessed through ssh-tunnels
 PORTS="80:80 443:443 $PORT_SSH:22"
 
+### SMTP server for sending notifications. You can build an SMTP server
+### as described here:
+### https://github.com/docker-scripts/postfix/blob/master/INSTALL.md
+### Comment out if you don't have a SMTP server and want to use
+### a gmail account (as described below).
+SMTP_SERVER=smtp.example.org
+SMTP_DOMAIN=example.org
+
 ### Gmail account for notifications. This will be used by ssmtp.
 ### You need to create an application specific password for your account:
 ### https://www.lifewire.com/get-a-password-to-access-gmail-by-pop-imap-2-1171882
